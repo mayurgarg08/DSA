5169604b14423f327bb28fece01123395d5ca2ce
mayurgarg08 <mayurgarg2005@gmail.com>
2024-12-25 12:42:13 +0000

    Sync LeetCode submission - Find Peak Element (java)

---
+++ problems/find_peak_element/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start < end) {
+            int mid = (start+end)/2;
+            if(arr[mid]<arr[mid+1]) {
+                //you are in the increasing part of the array
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+
+        }
+        return start;
+    }
+}
