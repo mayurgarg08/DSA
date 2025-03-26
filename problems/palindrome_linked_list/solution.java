d5aa56464ff171596a10337e773ecffc48411516
mayurgarg08 <mayurgarg2005@gmail.com>
2025-03-26 19:18:57 +0000

    Sync LeetCode submission - Palindrome Linked List (java)

---
+++ problems/palindrome_linked_list/solution.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+       ListNode fast = head;
+       ListNode slow = head;
+
+       while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+       }
+       return slow;
+    }
+    public ListNode reverseList(ListNode head) {
+        if(head == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+      
+       while(present != null) {
+         present.next = prev;
+         prev = present;
+         present = next;
+         
+         if(next != null) {
+            next = next.next;
+         }
+       }
+       return prev;
+    }
+    
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode headSecond = reverseList(mid);
+
+        while(head != null && headSecond != null) {
+            if(head.val != headSecond.val) {
+                break;
+            }
+
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+
+        reverseList(headSecond);
+
+        return head == null || headSecond == null;
+    }
+
+}
