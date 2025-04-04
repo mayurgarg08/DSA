f67f5ea514cec24de60bb1ae709689256236cfb2
mayurgarg08 <mayurgarg2005@gmail.com>
2025-04-04 14:42:34 +0000

    Sync LeetCode submission - Symmetric Tree (java)

---
+++ problems/symmetric_tree/solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) {
+            return false;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+         
+        while(!queue.isEmpty()) {
+          TreeNode left = queue.poll();
+          TreeNode right = queue.poll();
+         
+          if(left == null && right == null) {
+            continue;
+          } else if(left == null || right == null) {
+            return false;
+          } else if(left.val != right.val) {
+            return false;
+          }
+
+          queue.add(left.left);
+          queue.add(right.right);
+          queue.add(left.right);
+          queue.add(right.left);
+        }
+        return true;
+    }
+}
