5868009319c03e64e572a5216bc03d60f8e50f12
mayurgarg08 <mayurgarg2005@gmail.com>
2025-08-06 17:39:22 +0000

    Sync LeetCode submission - Majority Element (java)

---
+++ problems/majority_element/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if(nums.length == 1) {
+            return nums[0];
+        }
+       Map<Integer, Integer> map = new HashMap<>();
+       for(int num: nums) {
+         map.put(num, map.getOrDefault(num, 0) +1);
+
+         if(map.get(num) > nums.length/2) {
+            return num;
+         }
+       }
+     return -1;
+    }
+}
