70b4ed01e90bf513dede2c6d1bb1e53d363f3803
mayurgarg08 <mayurgarg2005@gmail.com>
2025-08-27 18:37:11 +0000

    Sync LeetCode submission - Heaters (java)

---
+++ problems/heaters/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        if(houses == null || houses.length == 0 || heaters == null || heaters.length == 0){
+            return 0;
+        }
+        
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        
+        int n = houses.length;
+        int m = heaters.length; 
+        
+        int i = 0;
+        int j = 0;
+        
+        int res = 0;
+        while(i < n && j < m){
+            
+            int dist1 = Math.abs(heaters[j] - houses[i]);
+            int dist2 = Integer.MAX_VALUE;
+            if(j + 1 < m){
+                dist2 = Math.abs(heaters[j + 1] - houses[i]);
+            }
+            if(dist1 < dist2){
+                res = Math.max(res, dist1);
+                i++;
+        
+            }else{
+                j++;
+            }
+        }
+        
+        return res;
+        
+    }
+}
