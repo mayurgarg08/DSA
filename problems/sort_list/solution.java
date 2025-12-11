92f3e8b008c6dcff2bc28279829a30dd854926ca
mayurgarg08 <mayurgarg2005@gmail.com>
2025-12-11 17:47:20 +0000

    Sync LeetCode submission - Sort List (java)

--- problems/sort_list/solution.java
+++ problems/sort_list/solution.java
@@ -9,42 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) {
-            return head;
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode mid = getMid(head);
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-
-        return merge(left, right);
+        return slow;
     }
 
-    private ListNode merge(ListNode list1, ListNode list2) {
-        ListNode reshead = new ListNode();
-        ListNode temp = reshead;
-     while(list1 != null && list2 != null) {
-        if(list1.val < list2.val) {
-            temp.next = list1;
-            list1 = list1.next;
-            temp = temp.next;
-        } else {
-            temp.next = list2;
-            list2 = list2.next;
-            temp = temp.next;
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        while(head1 != null && head2 != null) {
+            if(head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
         }
-     }
-     temp.next = (list1 != null) ? list1 : list2;
-      return reshead.next;
-   }
-  private ListNode getMid(ListNode head) {
-       ListNode midprev = null;
-       while(head != null && head.next != null) {
-        midprev = (midprev == null) ? head : midprev.next;
-        head = head.next.next;
-       }
-       ListNode mid = midprev.next;
-       midprev.next = null;
-       return mid;
-  }
+        if(head1 != null) tail.next = head1;
+        else tail.next = head2;
+        
+       return dummy.next; 
+    } 
+
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode mid = findMid(head);
+        ListNode head2 = mid.next;
+        mid.next = null;
+        ListNode list1 = sortList(head);
+        ListNode list2 = sortList(head2);
+        return merge(list1, list2);  
+    }
 }
