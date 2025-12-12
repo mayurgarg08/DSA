2787aeea305d3cc5f67c1febdd96e5a7656a4a80
mayurgarg08 <mayurgarg2005@gmail.com>
2025-12-12 15:08:13 +0000

    Sync LeetCode submission - Rotate List (java)

--- problems/rotate_list/solution.java
+++ problems/rotate_list/solution.java
@@ -9,29 +9,36 @@
  * }
  */
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        if(k < 1 || head == null || head.next == null) {
-            return head;
-        }
-
-        int length = 1;
+    private ListNode nthNode(ListNode head, int k) {
+        int count = 1;
         ListNode temp = head;
-
-        while(temp.next != null) {
+        while(temp != null && count != k) {
             temp = temp.next;
-            length++;
+            count++;
         }
-         temp.next = head;
-         int rotations = k % length;
-         int skip = length - rotations;
-
-         ListNode newLast = head;
-
-        for(int i = 0; i< skip-1; i++) {
-            newLast = newLast.next;
-        }
-         head = newLast.next;
-         newLast.next = null;      
-      return head;
+        return temp;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+      if(head == null || head.next == null || k == 0) {
+        return head;
+      }
+      int count = 1;
+      ListNode temp = head;
+      while(temp.next != null) {
+        temp = temp.next;
+        count++;
+      }
+      k = k%count;
+      
+      if(k == 0) {
+        return head;
+      }
+      temp.next = head;
+      ListNode nth = nthNode(head, count-k);
+ 
+      head = nth.next;
+      nth.next = null;
+      
+     return head;
     }
 }
