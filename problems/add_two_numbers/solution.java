6aee6b6f27ae3c47425a9f19a1d82bf777ad5e10
mayurgarg08 <mayurgarg2005@gmail.com>
2025-12-12 09:09:24 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- problems/add_two_numbers/solution.java
+++ problems/add_two_numbers/solution.java
@@ -10,28 +10,33 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode();
-        ListNode res = dummy;
-        int total = 0, carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            total = carry;
-
-            if (l1 != null) {
-                total += l1.val;
-                l1 = l1.next;
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        int carry = 0;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while (temp1 != null || temp2 != null || carry != 0) {
+            int sum = (temp1 != null? temp1.val: 0)+(temp2 != null? temp2.val: 0)+carry;
+            if(sum >= 10) {
+                ListNode node = new ListNode(sum%10);
+                temp.next = node;
+                carry = sum/10;
+                temp = node;
+            } else {
+                ListNode node = new ListNode(sum);
+                temp.next = node;
+                carry = 0;
+                temp = node;
             }
-            if (l2 != null) {
-                total += l2.val;
-                l2 = l2.next;
+            if(temp1 == null && temp2 == null && carry!= 0) {
+                temp.next = new ListNode(carry);
+                carry = 0;
             }
-
-            int num = total % 10;
-            carry = total / 10;
-            dummy.next = new ListNode(num);
-            dummy = dummy.next;
+            if(temp1 != null)
+            temp1 = temp1.next;
+            if(temp2 != null)
+            temp2 = temp2.next;
         }
-
-        return res.next;        
+        return dummy.next;
+        }
     }
-}
