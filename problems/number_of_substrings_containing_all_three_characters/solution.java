25f6870ab7e7ba8e1de4cb3a5e4dbeafbdca6d67
mayurgarg08 <mayurgarg2005@gmail.com>
2025-12-21 13:42:51 +0000

    Sync LeetCode submission - Number of Substrings Containing All Three Characters (java)

---
+++ problems/number_of_substrings_containing_all_three_characters/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int[] lastSeen = new int[26];
+        Arrays.fill(lastSeen, -1);
+        int count = 0;
+        for(int i = 0; i< s.length(); i++) {
+            lastSeen[s.charAt(i)-'a'] = i;
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                count += 1+Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
+            }
+        }
+        return count;
+    }
+}
