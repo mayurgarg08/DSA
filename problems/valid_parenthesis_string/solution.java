9b3605642637871e4822c983ff84bc43b427c131
mayurgarg08 <mayurgarg2005@gmail.com>
2026-01-20 11:09:43 +0000

    Sync LeetCode submission - Valid Parenthesis String (java)

---
+++ problems/valid_parenthesis_string/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int max = 0;
+        int min = 0;
+
+        for(int i = 0; i< s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                min++;
+                max++;
+            } else if(s.charAt(i) == ')') {
+                min--;
+                max--;
+            } else {
+                min--;
+                max++;
+            }
+            if(max < 0) return false;
+            if(min < 0) min = 0;
+        }
+        return min == 0;
+    }
+}
