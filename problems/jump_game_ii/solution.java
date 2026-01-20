f09e67db5161b418485d1b76335a1cbe87e56d42
mayurgarg08 <mayurgarg2005@gmail.com>
2026-01-20 13:04:38 +0000

    Sync LeetCode submission - Jump Game II (java)

---
+++ problems/jump_game_ii/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+       int ans = 0;
+       int currentEnd = 0;
+       int farthest = 0;
+       for(int i = 0; i < nums.length-1; i++) {
+        farthest = Math.max(farthest, i+nums[i]);
+
+        if(i == currentEnd) {
+            ans++;
+            currentEnd = farthest;
+        }
+       } 
+       return ans;
+    }
+}
