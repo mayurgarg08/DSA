08177e53cff708fe7baf2f209c35e047ac750353
mayurgarg08 <mayurgarg2005@gmail.com>
2026-01-21 12:37:37 +0000

    Sync LeetCode submission - Merge Intervals (java)

--- problems/merge_intervals/solution.java
+++ problems/merge_intervals/solution.java
@@ -1,19 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        if (intervals.length == 0) return new int[0][0];
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         List<int[]> merged = new ArrayList<>();
-        int[] prev = intervals[0];
 
-        for(int i = 1; i< intervals.length; i++) {
-            if(intervals[i][0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], intervals[i][1]);
-            } else {
-                merged.add(prev);
-                prev = intervals[i];
+        merged.add(new int[]{intervals[0][0], intervals[0][1]});
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            int[] lastMerged = merged.get(merged.size() - 1);
+            int currentStart = intervals[i][0];
+            int currentEnd   = intervals[i][1];
+
+            if (currentStart <= lastMerged[1]) {
+                lastMerged[1] = Math.max(lastMerged[1], currentEnd);
+            }
+            else {
+                merged.add(new int[]{currentStart, currentEnd});
             }
         }
-        merged.add(prev);
-        return merged.toArray(new int [merged.size()][]);
+
+        return merged.toArray(new int[merged.size()][]);
     }
 }
