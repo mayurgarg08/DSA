09acbd1a73fcac1e0cf6d5b8b38b861893c0217e
mayurgarg08 <mayurgarg2005@gmail.com>
2026-01-29 07:52:41 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (java)

--- problems/binary_tree_maximum_path_sum/solution.java
+++ problems/binary_tree_maximum_path_sum/solution.java
@@ -14,25 +14,18 @@
  * }
  */
 class Solution {
-    int ans = Integer.MIN_VALUE;
-    public int maxPathSum(TreeNode root) {
-        helper(root);
-        return ans;
+    private int maxPath(TreeNode node, int[] maxValue) {
+        if(node == null) return 0;
+        int left = Math.max(0, maxPath(node.left, maxValue));
+        int right = Math.max(0, maxPath(node.right, maxValue));
+
+        maxValue[0] = Math.max(maxValue[0], node.val + left + right);
+        return Math.max(left, right)+node.val;
     }
-    public int helper(TreeNode node) {
-        if(node == null) {
-            return 0;
-        }
-
-        int left = helper(node.left);
-        int right = helper(node.right);
-
-        left = Math.max(0, left);
-        right = Math.max(0, right);
-
-        int pathSum = left + right + node.val;
-        ans = Math.max(ans, pathSum);
-
-        return Math.max(left, right) + node.val;
+    public int maxPathSum(TreeNode root) {
+        int maxValue[] = new int[1];
+        maxValue[0] = Integer.MIN_VALUE;
+        maxPath(root, maxValue);
+        return maxValue[0];
     }
 }
