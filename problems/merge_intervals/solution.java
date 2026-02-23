7bb4a4b014b9f60ee9e5322b1236a86e86240177
mayurgarg08 <mayurgarg2005@gmail.com>
2026-02-23 21:28:38 +0000

    Sync LeetCode submission - Merge Intervals (java)

--- problems/merge_intervals/solution.java
+++ problems/merge_intervals/solution.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 0) return new int[0][0];
+        if(intervals.length == 0) return new int[0][0];
+        List<int[]> ans = new ArrayList<>();
+       int n = intervals.length;
+       int m = intervals[0].length;
+       Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+       ans.add(intervals[0]);
+       for(int i = 1; i< n; i++) {
+          if(ans.get(ans.size()-1)[1] < intervals[i][0]) {
+            ans.add(intervals[i]);
+          } else {
+            int end = Math.max(ans.get(ans.size()-1)[1], intervals[i][1]);
+            int start = Math.min(ans.get(ans.size()-1)[0], intervals[i][0]);
 
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
-        List<int[]> merged = new ArrayList<>();
-
-        merged.add(new int[]{intervals[0][0], intervals[0][1]});
-
-        for (int i = 1; i < intervals.length; i++) {
-
-            int[] lastMerged = merged.get(merged.size() - 1);
-            int currentStart = intervals[i][0];
-            int currentEnd   = intervals[i][1];
-
-            if (currentStart <= lastMerged[1]) {
-                lastMerged[1] = Math.max(lastMerged[1], currentEnd);
-            }
-            else {
-                merged.add(new int[]{currentStart, currentEnd});
-            }
-        }
-
-        return merged.toArray(new int[merged.size()][]);
+            ans.remove(ans.size() - 1);
+            ans.add(new int[] {start, end});
+          }
+       }
+       int k = ans.size();
+       int[][] result = new int[k][2];
+       for(int i = 0; i< k; i++) {
+         result[i] = ans.get(i);
+       }
+       return result;
     }
 }
