bf3293e710b2b94337ca0f47799574b5cb963134
mayurgarg08 <mayurgarg2005@gmail.com>
2026-02-23 17:12:29 +0000

    Sync LeetCode submission - Majority Element II (java)

--- problems/majority_element_ii/solution.java
+++ problems/majority_element_ii/solution.java
@@ -1,17 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        for(int num: nums) {
-            map.put(num, map.getOrDefault(num, 0) +1);
-
-            if(map.get(num) > nums.length/3) {
-                if(!list.contains(num)) {
-                   list.add(num);
-                }
-                
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+         int cnt1 = 0;
+         int cnt2 = 0;
+         int el1 = Integer.MIN_VALUE;
+         int el2 = Integer.MIN_VALUE;
+         
+         for(int i = 0; i< nums.length; i++) {
+            if(cnt1 == 0 && el2 != nums[i]) {
+                el1 = nums[i];
+                cnt1 = 1;
+            } else if(cnt2 == 0 && el1 != nums[i]) {
+                el2 = nums[i];
+                cnt2 = 1; 
             }
-        }
-        return list;
+            else if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
+         }   
+         int count1 = 0;
+         int count2 = 0;
+
+         for(int i = 0; i< nums.length; i++) {
+            if(nums[i] == el1) {
+                count1++;
+            } else if(nums[i] == el2) {
+                count2++;
+            }
+         }
+         if(count1 > Math.floor(n/3)) ans.add(el1);
+         if(count2 > Math.floor(n/3)) ans.add(el2);
+         return ans;
     }
 }
