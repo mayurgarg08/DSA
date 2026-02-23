75bf5e5418ae94797b5fe6a7bda15129574d0d0a
mayurgarg08 <mayurgarg2005@gmail.com>
2026-02-23 17:50:34 +0000

    Sync LeetCode submission - 3Sum (java)

--- problems/3sum/solution.java
+++ problems/3sum/solution.java
@@ -1,38 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-         Set<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
-       for(int i = 0; i<nums.length-2; i++) {
-         if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+        for(int i = 0; i < n-2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j < k) {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0) {
+                   ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                   j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                } else if(sum < 0) {
+                    j++;
+                }  else {
+                    k--;
+                }
             }
-         
-         int left = i+1;
-         int right = nums.length-1;
-         while(left<right) {
-            
-            if(nums[i] + nums[left] + nums[right] == 0) {
-             List<Integer> triplet = Arrays.asList(nums[i], nums[left], nums[right]);
-             Collections.sort(triplet); 
-             set.add(triplet);
-             if(left<right && nums[left] == nums[left+1]) {
-                left++;
-             }
-             if(left<right && nums[right] == nums[right-1]) {
-                right--;
-             }
-             left++;
-             right--;
-            } else if(nums[i] + nums[left] + nums[right] < 0) {
-                left++;
-            } else {
-                right--;
-            }
-            
-         }
-       }
-       ans.addAll(set);
+        }
         return ans;
     }
 }
