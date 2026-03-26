a412a59b0860a761119721a6a15d6bc98535f743
mayurgarg08 <mayurgarg2005@gmail.com>
2026-03-26 19:43:21 +0000

    Sync LeetCode submission - Minimum Path Sum (java)

--- problems/minimum_path_sum/solution.java
+++ problems/minimum_path_sum/solution.java
@@ -1,31 +1,31 @@
 class Solution {
-
     public int minPathSum(int[][] grid) {
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int[][] dp = new int[n][m];
-
-        return dfs(grid, 0, 0, dp);
+       int n = grid.length;
+       int m = grid[0].length;
+       int[] prev = new int[m];
+       for(int row = 0; row < n; row++) {
+        int[] curr = new int[m];
+        for(int col = 0; col < m; col++) {
+            if(row == 0 && col == 0) {
+                curr[col] = grid[0][0];
+                continue;
+            }
+            int left = Integer.MAX_VALUE, up = Integer.MAX_VALUE;
+            if(col > 0) left = grid[row][col] + curr[col-1];
+            if(row > 0) up = grid[row][col] + prev[col];
+            curr[col] = Math.min(left, up); 
+        }
+        prev = curr;
+       }
+       return prev[m-1];
     }
-
-    private int dfs(int[][] grid, int i, int j, int[][] dp) {
-
-        if(i >= grid.length || j >= grid[0].length)
-            return Integer.MAX_VALUE;
-
-        if(i == grid.length-1 && j == grid[0].length-1)
-            return grid[i][j];
-
-        if(dp[i][j] != 0)
-            return dp[i][j];
-
-        int right = dfs(grid, i, j+1, dp);
-        int down  = dfs(grid, i+1, j, dp);
-
-        dp[i][j] = grid[i][j] + Math.min(right, down);
-
-        return dp[i][j];
-    }
+    // private int helper(int row, int col, int[][] dp, int[][] grid) {
+    //    if(row == 0 && col == 0) return grid[0][0];
+    //    if(row < 0 || col < 0) return Integer.MAX_VALUE;
+    //    if(dp[row][col] != -1) return dp[row][col];
+    //    int left = Integer.MAX_VALUE, up = Integer.MAX_VALUE;
+    //    if(col > 0) left = grid[row][col] + helper(row, col-1, dp, grid);
+    //    if(row > 0) up = grid[row][col] + helper(row-1, col, dp, grid);
+    //    return dp[row][col] = Math.min(left, up); 
+    // }
 }
