1c6609daa9cb5702837eb9023a49b24c33cb9d4a
mayurgarg08 <mayurgarg2005@gmail.com>
2026-03-26 09:20:45 +0000

    Sync LeetCode submission - Frog Jump II (java)

---
+++ problems/frog_jump_ii/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxJump(int[] stones) {
+        int n = stones.length;
+        if(n == 1) return 0;
+        if(n == 2) return Math.abs(stones[1] - stones[0]);
+        int ans = 0;
+        
+        for(int i = 2; i < n; i++) {
+            ans = Math.max(ans, stones[i] - stones[i-2]);
+        }
+        
+        return ans;
+    }
+}
