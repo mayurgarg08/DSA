91e9872b8b4ce85fc1eac2c8fc3a6bdac3b77f01
mayurgarg08 <mayurgarg2005@gmail.com>
2026-03-31 14:42:14 +0000

    Sync LeetCode submission - Minimum Insertion Steps to Make a String Palindrome (java)

---
+++ problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int lcs(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[] prev = new int[m+1];
+        for(int idx1 = 1; idx1 <= n; idx1++) {
+            int[] curr = new int[m+1];
+            for(int idx2 = 1; idx2 <= m; idx2++) {
+                
+                 if(text1.charAt(idx1-1) == text2.charAt(idx2-1)) {
+                     curr[idx2] = 1 + prev[idx2-1];
+                 }
+                 else curr[idx2] = Math.max(0 + prev[idx2], 0 + curr[idx2-1]);
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+    public int longestPalindromeSubseq(String s) {   
+       String reversed = new StringBuilder(s).reverse().toString();
+       int ans = lcs(s, reversed);
+       return ans;
+    }
+    public int minInsertions(String s) {
+        int n = s.length();
+        return n-longestPalindromeSubseq(s);
+    }
+}
