0ee933f1d5b0187c2c43613f0d16adb3d221669f
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-01 10:54:35 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (java)

--- problems/best_time_to_buy_and_sell_stock_ii/solution.java
+++ problems/best_time_to_buy_and_sell_stock_ii/solution.java
@@ -1,17 +1,33 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int profit = 0;
-        int[] diff = new int[n-1];
-        for(int i = 0; i < n-1; i++) {
-            diff[i] = prices[i+1]-prices[i];
-        }
-        for(int i = 0; i < n-1; i++) {
-            if(diff[i] < 0) {
-               continue;
+        int[] front = new int[2];
+        front[0] = 0;
+        front[1] = 0;
+       for(int idx = n-1; idx >= 0; idx--) {
+        int[] curr = new int[2];
+        for(int buy = 0; buy < 2; buy++) {
+            int profit;
+             if(buy == 1) {
+            profit = Math.max(-prices[idx] + front[0], 0 + front[1]);
+            } else {
+            profit = Math.max(prices[idx] + front[1], 0 + front[0]);
             }
-            profit += diff[i];
+            curr[buy] = profit;
         }
-        return profit;
+         front = curr;
+       } 
+       return front[1];
     }
+    // private int helper(int idx, int buy, int[][] dp, int[] prices) {
+    //     if(idx == prices.length) return 0;
+    //     if(dp[idx][buy] != -1) return dp[idx][buy];
+    //     int profit;
+    //     if(buy == 1) {
+    //         profit = Math.max(-prices[idx] + helper(idx+1, 0, dp, prices), 0 + helper(idx+1, 1, dp, prices));
+    //     } else {
+    //         profit = Math.max(prices[idx] + helper(idx+1, 1, dp, prices), 0 + helper(idx+1, 0, dp, prices));
+    //     }
+    //     return dp[idx][buy] = profit;
+    //}
 }
