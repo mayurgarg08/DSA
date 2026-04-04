560f5e3c75b91ce64c2559bf165c93bfea7b5cbf
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-04 17:15:24 +0000

    Sync LeetCode submission - Trionic Array I (java)

---
+++ problems/trionic_array_i/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if(n < 4) return false;
+
+        int i = 0;
+
+        while(i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        int p = i;
+
+        while(i + 1 < n && nums[i] > nums[i + 1]) {
+            i++;
+        }
+        int q = i;
+
+        while(i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1 && p > 0 && q > p && q < n - 1;
+    }
+}
