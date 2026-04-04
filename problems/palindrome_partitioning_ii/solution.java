9db28d1d3169f649379177eae7c733bc03cdd71b
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-04 12:45:47 +0000

    Sync LeetCode submission - Palindrome Partitioning II (java)

---
+++ problems/palindrome_partitioning_ii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    private boolean isPalindrome(int i, int j, String s) {
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[n] = 0;
+        for(int i = n-1; i >= 0; i--) {
+             int mini = Integer.MAX_VALUE;
+
+           for(int j = i; j < n ; j++) {
+                if(isPalindrome(i, j, s)) {
+                  int cuts = 1 + dp[j+1];
+                  mini = Math.min(mini, cuts);
+                }
+            }
+            dp[i] = mini;    
+        }
+        return dp[0]-1;
+    }
+}
