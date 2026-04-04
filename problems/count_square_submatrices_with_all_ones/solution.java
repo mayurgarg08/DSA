76a83ba95a242bce663fa8ece3b1c7ce84bab3fc
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-04 13:29:10 +0000

    Sync LeetCode submission - Count Square Submatrices with All Ones (java)

--- problems/count_square_submatrices_with_all_ones/solution.java
+++ problems/count_square_submatrices_with_all_ones/solution.java
@@ -1,26 +1,27 @@
-public class Solution {
+class Solution {
     public int countSquares(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int total = 0;
+       int n = matrix.length;
+       int m = matrix[0].length;
 
-        int[][] dp = new int[m][n];
+       int[][] dp = new int[n][m];
+ 
+       for(int i = 0; i< n; i++) dp[i][0] = matrix[i][0];
+       for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 1) {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = Math.min(
-                            Math.min(dp[i - 1][j], dp[i][j - 1]),
-                            dp[i - 1][j - 1]
-                        ) + 1;
-                    }
-                    total += dp[i][j];
-                }
-            }
+       for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+           if(matrix[i][j] == 0) dp[i][j] = 0;
+           else {
+            dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+           }
         }
-        return total;
+       }   
+       int sum = 0;
+       for(int i = 0; i < n; i++) {
+        for(int j = 0; j <m; j++) {
+             sum += dp[i][j];
+        }
+       }
+       return sum;
     }
 }
