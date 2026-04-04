167c38559c20caff06803c8146a1ee43edec7cc7
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-04 07:00:39 +0000

    Sync LeetCode submission - Burst Balloons (java)

---
+++ problems/burst_balloons/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n+2];
+        arr[0] = 1;
+        for(int i = 0; i < n; i++) {
+            arr[i+1] = nums[i];
+        }
+        arr[n+1] = 1;
+        int[][] dp = new int[n+2][n+2];
+        
+
+        for(int i = n; i>= 1; i--) {
+            for(int j = 1; j <= n; j++) {
+                if(i > j) continue;
+                int maxi = Integer.MIN_VALUE;
+              for(int idx = i; idx <= j; idx++) {
+            int cost = arr[i-1] * arr[idx] * arr[j+1] + dp[i][idx-1] + dp[idx+1][j];
+            maxi = Math.max(maxi, cost);
+        }
+           dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
+    }
+}
