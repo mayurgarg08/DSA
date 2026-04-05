9ce2f4540ffbed5c168bbbbcf8934f8c6d3e6169
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-05 14:10:44 +0000

    Sync LeetCode submission - Maximal Square (java)

---
+++ problems/maximal_square/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int n = matrix.length;
+       int m = matrix[0].length;
+
+       int[][] dp = new int[n][m];
+ 
+       for(int i = 0; i< n; i++) dp[i][0] = matrix[i][0]-'0';
+       for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j]-'0';
+
+       for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+           if(matrix[i][j] == '0') dp[i][j] = 0;
+           else {
+            dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+           }
+        }
+       }   
+     int maxi = 0;
+     for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+           maxi = Math.max(maxi, dp[i][j]);
+        }
+     }
+     return maxi * maxi;
+    }
+}
