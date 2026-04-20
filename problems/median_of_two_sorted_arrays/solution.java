ecac8a7d34e245523ee481f038b469d89c5f081c
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-20 15:41:07 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (java)

--- problems/median_of_two_sorted_arrays/solution.java
+++ problems/median_of_two_sorted_arrays/solution.java
@@ -1,16 +1,27 @@
-import java.util.Arrays;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] mergedArray = new int[nums1.length + nums2.length];
-
-        System.arraycopy(nums1, 0, mergedArray, 0, nums1.length);
-        System.arraycopy(nums2, 0, mergedArray, nums1.length, nums2.length);
-         
-        Arrays.sort(mergedArray); 
-        if(mergedArray.length % 2 != 0) {
-            return mergedArray[(mergedArray.length)/2];
-        } else {
-            return (mergedArray[(mergedArray.length-1)/2] + mergedArray[(mergedArray.length)/2]) / 2.0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1)/2;
+        int n = n1+n2;
+        while(low <= high) {
+            int mid1 = (low+high)/2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1-1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1) {
+                if(n%2 == 1) return Math.max(l1, l2);
+                return ((double)(Math.max(l1, l2) + Math.min(r1, r2)))/2.0;
+            }
+            else if(l1 > r2) high = mid1-1;
+            else low = mid1 + 1;
         }
+        return 0;
     }
 }
