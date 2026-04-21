656fa88acb0dc4110e534277c4deb4c6886eab0d
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-21 18:24:28 +0000

    Sync LeetCode submission - Arithmetic Slices (java)

---
+++ problems/arithmetic_slices/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        if(n < 3) return 0;
+
+        int curr = 0;
+        int total = 0;
+
+        for(int i = 2; i < n; i++) {
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                curr += 1;
+                total += curr;
+            } else {
+                curr = 0;
+            }
+        }
+
+        return total;
+    }
+}
