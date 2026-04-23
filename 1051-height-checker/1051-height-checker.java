2a074315aa8d90029fafe4a9761284cd19824bea
mayurgarg08 <mayurgarg2005@gmail.com>
2026-04-24 01:43:39 +0530

    Time: 4 ms (78.02%), Space: 43.7 MB (14.89%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] temp = new int[n];
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            temp[i] = heights[i];
+        }
+        Arrays.sort(heights);
+        for(int i = 0; i < n; i++) {
+            if(heights[i] != temp[i]) count++;
+        }
+        return count;
+    }
+}
