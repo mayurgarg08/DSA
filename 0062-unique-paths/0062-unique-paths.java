637e82683e903668c457b3b854ea43fa7eec5ca0
mayurgarg08 <mayurgarg2005@gmail.com>
2026-06-23 01:18:24 +0530

    Time: 1 ms (14.01%), Space: 41.9 MB (91.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,27 @@
 class Solution {
-    private int helper(int i, int j, int[][] dp) {
-       if(i == 0 && j == 0) return 1;
-         if(i < 0 || j < 0) return 0;
-         if(dp[i][j] != -1) return dp[i][j];
-         int up = helper(i-1,j, dp);
-         int left = helper(i, j-1, dp);
-         return dp[i][j] = up + left;
-    }
+    // private int helper(int i, int j, int[][] dp) {
+    //    if(i == 0 && j == 0) return 1;
+    //      if(i < 0 || j < 0) return 0;
+    //      if(dp[i][j] != -1) return dp[i][j];
+    //      int up = helper(i-1,j, dp);
+    //      int left = helper(i, j-1, dp);
+    //      return dp[i][j] = up + left;
+    // }
     public int uniquePaths(int m, int n) {
-         int[][] dp = new int[m][n];
+         int[] prev = new int[n];
          for(int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+            int[] curr = new int[n];
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) curr[j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = curr[j-1];
+                    curr[j] = left + up;
+                }
+            }
+            prev = curr;
          }
-         return helper(m-1, n-1, dp);
+         return prev[n-1];
     }
 }
