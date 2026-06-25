81e7e43926995d46622198996639c285a92840a2
mayurgarg08 <mayurgarg2005@gmail.com>
2026-06-25 13:48:49 +0530

    Time: 13 ms (91.53%), Space: 46.6 MB (41.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -12,20 +12,22 @@ class Solution {
     // }
     public int coinChange(int[] coins, int amount) {
           int n = coins.length;
-          int[][] dp = new int[n][amount+1];
+          int[] prev = new int[amount+1];
+          int[] curr = new int[amount+1];
           for(int target = 0; target <= amount; target++) {
-            if(target % coins[0] == 0) dp[0][target] = target/coins[0];
-            else dp[0][target] = (int)(1e9);
+            if(target % coins[0] == 0) prev[target] = target/coins[0];
+            else prev[target] = (int)(1e9);
           }
           for(int idx = 1; idx < n; idx++) {
             for(int target = 0; target <= amount; target++) {
-                int notTake = dp[idx-1][target];
+                int notTake = prev[target];
                 int take = (int)1e9;
-                if(coins[idx] <= target) take = 1 + dp[idx][target-coins[idx]];
-                dp[idx][target] = Math.min(take, notTake);
+                if(coins[idx] <= target) take = 1 + curr[target-coins[idx]];
+                curr[target] = Math.min(take, notTake);
             }
+            prev = curr;
           }
-          int ans = dp[n-1][amount];
+          int ans = prev[amount];
           if(ans >= (int)(1e9)) return -1;
           return ans; 
     }
