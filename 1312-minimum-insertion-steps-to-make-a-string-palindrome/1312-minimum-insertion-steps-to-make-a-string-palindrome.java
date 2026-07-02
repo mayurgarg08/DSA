2b99678fde200b04dfdb663ade51b2531d3c6a7f
mayurgarg08 <mayurgarg2005@gmail.com>
2026-07-03 00:05:44 +0530

    Time: 24 ms (66.44%), Space: 46.7 MB (78.18%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -2,16 +2,17 @@ class Solution {
     private int lcs(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int j = 0; j <= m; j++) dp[0][j] = 0;
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        int[] prev = new int[m+1];
+        Arrays.fill(prev, 0);
         for(int idx1 = 1; idx1 <= n; idx1++) {
+            int[] curr = new int[m+1];
             for(int idx2 = 1; idx2 <= m; idx2++) {
-                if(s.charAt(idx1-1) == t.charAt(idx2-1)) dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
-                else dp[idx1][idx2] = Math.max(dp[idx1-1][idx2], dp[idx1][idx2-1]);
+                if(s.charAt(idx1-1) == t.charAt(idx2-1)) curr[idx2] = 1 + prev[idx2 - 1];
+                else curr[idx2] = Math.max(prev[idx2], curr[idx2-1]);
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
     private String reverse(String s) {
         StringBuilder rev = new StringBuilder();
