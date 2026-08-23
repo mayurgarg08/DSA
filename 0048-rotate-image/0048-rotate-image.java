26c418fc07228b6223bfb55f59bf5de30cc1f202
mayurgarg08 <mayurgarg2005@gmail.com>
2026-08-24 01:47:05 +0530

    Time: 0 ms (100%), Space: 43.9 MB (16.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,17 +3,19 @@ class Solution {
         int temp = matrix[i][j];
         matrix[i][j] = matrix[j][i];
         matrix[j][i] = temp;
+        return;
     }
     private void reverse(int[] arr) {
         int low = 0;
         int high = arr.length-1;
-        while(low <= high) {
+        while(low < high) {
             int temp = arr[low];
             arr[low] = arr[high];
             arr[high] = temp;
             low++;
             high--;
         }
+        return;
     }
     public void rotate(int[][] matrix) {
         int n = matrix.length;
