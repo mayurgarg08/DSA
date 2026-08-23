5fa345038714fea1f5a79baca4c31392d2a11ad3
mayurgarg08 <mayurgarg2005@gmail.com>
2026-08-23 22:35:48 +0530

    Time: 0 ms (100%), Space: 44.2 MB (96.9%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,42 @@
 class Solution {
-    private void reverse(int[] arr, int low, int high) {
-        while(low <= high) {
-            swap(arr, low, high);
-            low++;
-            high--;
-        }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        return;
     }
-    private void swap(int[] arr, int low, int high) {
-        int temp = arr[low];
-        arr[low] = arr[high];
-        arr[high] = temp;
+     private void reverse(int[] nums, int i, int j) {
+        int start = i;
+        int end = j;
+        while(start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+        return;
     }
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-       int idx = -1;
-       for(int i = n-2; i >= 0; i--) {
-          if(nums[i] < nums[i+1]) {
-            idx = i;
-            break;
-          }
-       }
-       if(idx == -1) {
-        reverse(nums, 0, n-1);
-        return;
-       }
-       for(int i = n-1; i >= 0; i--) {
-        if(nums[i] > nums[idx]) {
-            swap(nums, i, idx);
-            break;
+        int idx1 = -1;
+        int idx2 = -1;
+        for(int i = n-2; i >= 0; i--) {
+            if(nums[i] < nums[i+1]) {
+                idx1 = i;
+                break;
+            }
         }
-       } 
-       reverse(nums, idx+1, n-1);
+        if(idx1 == -1) {
+            reverse(nums, 0, n-1);
+            return;
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+            if(nums[i] > nums[idx1]) {
+                idx2 = i;
+                break;
+            }
+        }
+        swap(nums, idx1, idx2);
+        reverse(nums, idx1 +1, n-1);
     }
 }
