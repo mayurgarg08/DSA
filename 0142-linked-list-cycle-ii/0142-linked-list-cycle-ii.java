9c429f17c209bb7c2aaae08580c7d8fb329305a8
Mayur Garg <mayurgarg2005@gmail.com>
2026-09-08 12:31:24 +0530

    Time: 0 ms (100%), Space: 46.5 MB (70.05%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -17,12 +17,12 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast) {
-               slow = head;
-               while(slow != fast) {
-                  slow = slow.next;
-                  fast = fast.next;
-               }
-               return slow;
+                slow = head;
+                while(slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
         }
         return null;
