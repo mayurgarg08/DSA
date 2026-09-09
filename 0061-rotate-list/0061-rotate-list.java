24b96943a865e6fd2725dc409054dea810b25889
Mayur Garg <mayurgarg2005@gmail.com>
2026-09-09 16:10:03 +0530

    Time: 0 ms (100%), Space: 44.4 MB (25.43%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,28 +9,35 @@
  * }
  */
 class Solution {
+    public int findLength(ListNode head) {
+        int count = 1;
+        ListNode curr = head;
+        while(curr != null) {
+            curr = curr.next;
+            count++;
+        }
+        return count-1;
+    }
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null) return head;
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode temp = head;
-        int len = 0;
-        while(temp != null) {
-            temp = temp.next;
-            len++;
-        }
-        k = k%len;
-        while(k > 0 && fast.next != null) {
-            fast = fast.next;
-            k--;
-        }
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        fast.next = head;
-        head = slow.next;
-        slow.next = null;
-        return head;
+      if(head == null || head.next == null || k == 0) return head;
+      int len = findLength(head);
+      k = k%len;
+      if(k == 0) return head;
+      ListNode slow = head;
+      ListNode fast = head;
+      int count = 0;
+      while(count != k) {
+        if(fast == null) return head;
+        fast = fast.next;
+        count++;
+      }
+      while(fast.next != null) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      ListNode newHead = slow.next;
+      slow.next = null;
+      fast.next = head;
+      return newHead;
     }
 }
