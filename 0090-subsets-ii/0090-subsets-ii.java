7ef3e9c9c3b4bcbe58865b97c30189be82793154
Mayur Garg <mayurgarg2005@gmail.com>
2026-09-10 21:28:01 +0530

    Time: 2 ms (99.74%), Space: 44.9 MB (76.27%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,18 +1,19 @@
 class Solution {
-    private void helper(int idx, int n, int[] nums, List<Integer> curr, List<List<Integer>> ans) {
-        ans.add(new ArrayList<>(curr));
-        for(int i = idx; i < n; i++) {
-            if(i != idx && nums[i] == nums[i-1]) continue;
-            curr.add(nums[i]);
-            helper(i+1, n, nums, curr, ans);
-            curr.remove(curr.size()-1);
+    private void helper(int index, int n, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(temp));
+
+        for(int i = index; i < n; i++) {
+            if(i > index && nums[i] == nums[i-1]) continue;
+            temp.add(nums[i]);
+            helper(i+1, n, nums, temp, ans);
+            temp.remove(temp.size()-1);
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(0, n, nums, new ArrayList<>(), ans);
-        return ans;
+       int n = nums.length;
+       List<List<Integer>> ans = new ArrayList<>();
+       helper(0, n, nums, new ArrayList<>(), ans);
+       return ans;
     }
 }
