0f549cb1141b4d689db758f76ca9243689fe27c9
Mayur Garg <mayurgarg2005@gmail.com>
2026-09-12 18:07:14 +0530

    Time: 389 ms (19.67%), Space: 42.9 MB (59.73%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,25 +1,21 @@
 class Solution {
-    private boolean isValid(char[][] board, int row, int col, char c) {
-        for(int i = 0; i < 9; i++) {
+    private boolean isPossible(int row, int col, char c, char[][] board) {
+        for(int i = 0; i  < 9; i++) {
             if(board[row][i] == c) return false;
             if(board[i][col] == c) return false;
-            if(board[3 * (row / 3) + i/3][3 * (col / 3) + i%3] == c) return false;
-         }
-         return true;
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i % 3] == c) return false;
+        }
+        return true;
     }
-    private boolean helper(int n, char[][] board) {
-        for(int row = 0; row < n; row++) {
-            for(int col = 0; col < n; col++) {
-
-                if(board[row][col] == '.') {
-
+    private boolean solve(char[][] board) {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') {
                     for(char c = '1'; c <= '9'; c++) {
-
-                        if(isValid(board, row, col, c)) {
-                            board[row][col] = c;
-
-                            if(helper(n, board)) return true;
-                            else board[row][col] = '.';
+                        if(isPossible(i, j, c, board)) {
+                            board[i][j] = c;
+                            if(solve(board) == true) return true;
+                            else board[i][j] = '.';
                         }
                     }
                     return false;
@@ -29,7 +25,6 @@ class Solution {
         return true;
     }
     public void solveSudoku(char[][] board) {
-        int n = board.length;
-        helper(n, board);
+        solve(board);
     }
 }
