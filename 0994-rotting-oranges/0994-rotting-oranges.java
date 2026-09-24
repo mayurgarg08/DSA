f4d9cdb947774da8805b95b5383b87673dfca073
Mayur Garg <mayurgarg2005@gmail.com>
2026-09-24 15:53:52 +0530

    Time: 2 ms (87.45%), Space: 44.3 MB (39.45%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,53 +1,46 @@
-class Pair {
-    int row;
-    int col;
-    int tm;
-    Pair(int row, int col, int tm) {
-        this.row = row;
-        this.col = col;
-        this.tm = tm;
-    }
-}
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length;
-        int[][] vis = new int[n][m];
-        Queue<Pair> q = new LinkedList<>();
-        int cntFresh = 0;
-        for(int i = 0; i< n; i++) {
+        int m  = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int vis[][] = new int[n][m];
+        int count = 0;
+        int fresh = 0;
+        int[] di = {-1, 0, 1, 0};
+        int[] dj = {0, 1, 0, -1};
+
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 2) {
-                    vis[i][j] = 2;
-                    q.add(new Pair(i, j, 0));
-                } else {
-                    vis[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                    vis[i][j] = 1;
                 }
-                if(grid[i][j] == 1) cntFresh++;
+                if(grid[i][j] == 1) fresh++;
             }
         }
-        int tm = 0;
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-        int cnt = 0;
         while(!q.isEmpty()) {
-           int r = q.peek().row;
-           int c = q.peek().col;
-           int t = q.peek().tm;
-           tm = Math.max(tm, t);
-           q.remove();
-           for(int i = 0; i < 4; i++) {
-              int nrow = r + drow[i];
-              int ncol = c + dcol[i];
+            int size = q.size();
+            boolean flag = false;
+            for(int i = 0; i < size; i++) {
+                 int[] curr = q.poll();
+                 int row = curr[0];
+                 int col = curr[1];
+                 
+                 for(int k = 0; k < 4; k++) {
+                    int drow = di[k] + row;
+                    int dcol = dj[k] + col;
 
-              if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2) {
-                  cnt++;
-                  q.add(new Pair(nrow, ncol, t+1));
-                  vis[nrow][ncol] = 2;
-              }
-           }
+                    if(drow >= 0 && drow < n && dcol >= 0 && dcol < m && vis[drow][dcol] == 0 && grid[drow][dcol] == 1) {
+                        q.offer(new int[]{drow, dcol});
+                        vis[drow][dcol] = 1;
+                        fresh--;
+                        flag = true;
+                    }
+                }
+            }
+            if(flag == true) count += 1; 
         }
-        if(cnt != cntFresh) return -1;
-        return tm;
+        return fresh == 0 ? count: -1;
     }
 }
